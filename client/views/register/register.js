f7e4397133ef9642c435a0cc6e50bad781345b3d
Jared Handra <jared.handra@gmail.com>
2015-10-25 17:41:24 -0400

    Added Front End Form Validation to Register Modal
    Added Front End Form Validation to Register Modal. Satisfies issue #15

--- client/views/register/register.js
+++ client/views/register/register.js
@@ -1,19 +1,36 @@
 Template.register.events({
    'submit #register-form' : function(e, t) {
      e.preventDefault();
+     var user;
+     // Collect data and validate it.
      var email = t.find('#account-email').value
-       , password = t.find('#account-password').value.trim()
-       , username = t.find('#account-username').value.trim();
-       //todo add validation
-     Accounts.createUser({username: username,  password : password, email: email}, function(err){
-         if (err) {
-           window.alert(err.reason);
-         } else {
-           document.getElementById("login-form").reset();
-           $('#signUp').modal('hide');
-         }
-       });
-
+      , username = t.find('#account-username').value.trim()
+      , password = t.find('#account-password').value.trim();
+     // Format User data into JSON
+     user = {
+       username: username,
+       password: password,
+       email: email
+     }
+     // Validate new users
+     if (user.username.length >=5) {
+       if (user.password.length >=6){
+         Accounts.createUser(user, function(err){
+             if (err) {
+               window.alert(err.reason);
+             } else {
+               document.getElementById("register-form").reset();
+               $('#signUp').modal('hide');
+             }
+           });
+       } else {
+         document.getElementById('error').className += 'alert alert-danger';
+         document.getElementById('error').innerHTML = 'Enter a valid password!';
+       }
+     } else {
+       document.getElementById('error').className += 'alert alert-danger';
+       document.getElementById('error').innerHTML = 'Enter a valid username!';
+     }
      return false;
    }
  });
