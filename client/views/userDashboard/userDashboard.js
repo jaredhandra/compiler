79c5d755e376ae3783efb4354fdae7b309d2339c
JoshFouchey <JoshFouchey@gmail.com>
2015-11-01 22:07:45 -0500

    Setup for Google picture

--- client/views/userDashboard/userDashboard.js
+++ client/views/userDashboard/userDashboard.js
@@ -10,3 +10,14 @@ Template.userDashboard.helpers({
      return Meteor.user().username || Meteor.user().profile.login || Meteor.user().profile.name
   }
 });
+
+Template.userDashboard.helpers({
+	avatarURL: function(){
+		if(Meteor.user().profile.avatar_url != null){
+			return  Meteor.user().profile.avatar_url;
+		}
+		if(Meteor.user().services.google.picture !=null){
+			return Meteor.user().services.google.picture;
+		}
+	}
+})
