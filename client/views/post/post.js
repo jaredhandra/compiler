4808af7282c7eb76c0632a447b91ee237920236d
Jared Handra <jared.handra@gmail.com>
2015-11-15 16:59:29 -0500

    individual questions formatting
    individual questions formatting

--- client/views/post/post.js
+++ client/views/post/post.js
@@ -1,27 +1,42 @@
 Template.post.events({
-  'submit .new-comment': function(event) {
-    var comment = event.target.commentText.value;
-    var user = Meteor.user();
-    var username = Meteor.user().username;
-    var questionId = findQuestionIdFromUrl(window.location.pathname);
-    var newComment = [
-    {"commentText":comment},
-    {"user":user},
-    {"username":username}
-    ];
-
-    Questions.update(
-      { _id: questionId},
-      { $push: { comments: newComment }}
-      )
-
-    document.getElementById("new-comment").reset();
-    return false;
-  }
+    'submit .new-comment': function (event) {
+        var comment = event.target.commentText.value;
+        var user = Meteor.user();
+        var username = Meteor.user().username;
+        var questionId = findQuestionIdFromUrl(window.location.pathname);
+        var newComment = {
+            "user": user,
+            "username": username,
+            "commentText": comment
+        };
+        Questions.update(
+            {_id: questionId},
+            {$push: {comments: newComment}}
+        )
+        document.getElementById("new-comment").reset();
+        return false;
+    }
 });
-
+Template.post.helpers({
+    questionDate: function () {
+        var date = new Date(Questions.findOne(this.createdAt).createdAt);
+        return moment(date).fromNow();
+    },
+    askerAvatarURL: function () {
+        var asker = Questions.findOne(this.createdAt).user;
+        if (asker.avatar != null) {
+            return asker.avatar;
+        }
+        if (asker.profile.avatar_url != null) {
+            return asker.profile.avatar_url;
+        }
+        if (asker.services.google.picture != null) {
+            return asker.services.google.picture;
+        }
+    }
+});
 //Couldn't get the question id from the dom
 //For now pulling it from the url..
-function findQuestionIdFromUrl(pathname){
-  return pathname.substring(10, pathname.length);
+function findQuestionIdFromUrl(pathname) {
+    return pathname.substring(10, pathname.length);
 }
