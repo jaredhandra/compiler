7fba1ee50b26707771819fe00448ae5f902a5bb2
JoshFouchey <JoshFouchey@gmail.com>
2015-11-15 17:02:18 -0500

    Trim data saved to questions/comments table

--- client/views/post/post.js
+++ client/views/post/post.js
@@ -1,12 +1,12 @@
 Template.post.events({
   'submit .new-comment': function(event) {
+    var userId = Meteor.user()._id;
     var comment = event.target.commentText.value;
-    var user = Meteor.user();
     var username = Meteor.user().username;
     var questionId = findQuestionIdFromUrl(window.location.pathname);
     var newComment = [
     {"commentText":comment},
-    {"user":user},
+    {"userId":userId},
     {"username":username}
     ];
 
