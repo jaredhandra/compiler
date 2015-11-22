b0d4b1566f1338f9db15ab9b232d4e936cab8179
JoshFouchey <JoshFouchey@gmail.com>
2015-11-22 12:27:03 -0500

    Adding autocorrect to categories textbox

--- client/views/questions/newQuestion.js
+++ client/views/questions/newQuestion.js
@@ -30,6 +30,11 @@ Template.newQuestion.events({
     return false;
   }
 });
+Template.newQuestion.helpers({
+  tags: function() {
+    return Tags.find().fetch().map(function(it){ return it.name; });
+  }
+});
 Template.newQuestion.rendered = function() {
 	Meteor.typeahead.inject($('.typeahead'));
 }
