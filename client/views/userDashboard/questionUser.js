09b5d308a0e5e8192c63fa6d2037aa264da42bfe
Josh Fouchey <JoshFouchey@gmail.com>
2016-02-25 15:47:42 -0500

    Fix to avatar in question table.

--- client/views/userDashboard/questionUser.js
+++ client/views/userDashboard/questionUser.js
@@ -1,11 +1,12 @@
 Template.questionUser.helpers({
-  userAvatar: function(userId) {
-    var asker = Meteor.users.findOne({userId});
+  userAvatar: function() {
+    var question = Questions.findOne(this._id);
+    var asker = Meteor.users.findOne(question.userId);
     if (asker.avatar != null) {
       return asker.avatar;
-    } else if (asker.profile.avatar_url != null) {
+    } else if (asker.profile != null && asker.profile.avatar_url != null) {
       return asker.profile.avatar_url;
-    } else if (asker.services.google.picture != null) {
+    } else if (asker.services != null && asker.services.google != null && asker.services.google.picture != null) {
         return asker.services.google.picture;
     } else {
       return null;
