e9ea4da830da8952d511e9d3cdeaf4ce91f85fa6
Jared Handra <jared.handra@gmail.com>
2016-02-28 00:38:07 -0500

    Fixed user profile duplicate issue
    Fixed user profile duplicate issue

--- client/views/userDashboard/questionUser.js
+++ client/views/userDashboard/questionUser.js
@@ -1,15 +1,52 @@
 Template.questionUser.helpers({
-  userAvatar: function() {
+  profile: function(){
     var question = Questions.findOne(this._id);
     var asker = Meteor.users.findOne(question.userId);
-    if (asker.avatar != null) {
-      return asker.avatar;
-    } else if (asker.profile != null && asker.profile.avatar_url != null) {
-      return asker.profile.avatar_url;
-    } else if (asker.services != null && asker.services.google != null && asker.services.google.picture != null) {
-        return asker.services.google.picture;
+    return asker;
+  },
+  username: function(){
+    return this.username;
+  },
+  userAvatar: function() {
+    // var question = Questions.findOne(this._id);
+    // var asker = Meteor.users.findOne(question.userId);
+    if (this.avatar != null) {
+      return this.avatar;
+    } else if (this.profile != null && this.profile.avatar_url != null) {
+      return this.profile.avatar_url;
+    } else if (this.services != null && this.services.google != null && this.services.google.picture != null) {
+        return this.services.google.picture;
     } else {
       return null;
+    }
+  },
+  githubAccount: function(userId){
+    if (this.services.github != null){
+      return this.profile.html_url;
+    }
+  },
+  emailAccount: function(userId){
+    if (this.email != null){
+      return this.email;
+    } else if(this.profile.email != null){
+      return this.profile.email;
+    } else if(this.services.google.family_name != null){
+      console.log(this.services.google.family_name);
+      return this.services.google.family_name;
+    }
+  },
+  both: function(){
+    if (this.services.github != null){
+      if (this.email != null){
+        return true;
+      } else if(this.profile.email != null){
+        return true;
+      } else if(this.services.google.family_name != null){
+        console.log(this.services.google.family_name);
+        return true;
+      } else {
+        return false;
+      }
     }
   }
 });
